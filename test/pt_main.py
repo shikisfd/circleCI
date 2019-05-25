9c0f38977d1b1b64bdc7e9d0c54bd7b17b579cd7
shikisfd <trex.sfd@gmail.com>
2019-05-25 16:20:31 +0900

    修正

--- test/pt_main.py
+++ test/pt_main.py
@@ -5,7 +5,7 @@ import sys
 print('test')
 
 
-result = subprocess.check_output( ["python", "dev\main.py", "data"] ).decode("UTF-8").strip()
+result = subprocess.check_output( ["python", "dev/main.py", "data"] ).decode("UTF-8").strip()
 if result == '1274383':
   print('OK')
 else:
