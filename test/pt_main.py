449f9b251ea689d409134aea6bcf052da5502418
shikisfd <trex.sfd@gmail.com>
2019-05-25 16:11:07 +0900

    test

--- test/pt_main.py
+++ test/pt_main.py
@@ -2,6 +2,9 @@ import subprocess
 import os
 import sys
 
+  print('test')
+
+
 result = subprocess.check_output( ["python", "dev\main.py", "data"] ).decode("UTF-8").strip()
 if result == '1274383':
   print('OK')
