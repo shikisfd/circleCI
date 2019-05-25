10d6161b0c802e1332c6b3b2f34fcb7f23c7ae44
shikisfd <trex.sfd@gmail.com>
2019-05-25 13:49:20 +0900

    実行確認

--- test/pt_main.py
+++ test/pt_main.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 result = subprocess.check_output( ["python", "dev\main.py", "data"] ).decode("UTF-8").strip()
-if result == 127483:
+if result == '1274383':
   print('OK')
 else:
-    print('OK')
+  print('NG')
